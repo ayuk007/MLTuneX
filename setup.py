191ec5a2deeedb2d19b35734d2c39c475f1f9488
ayuk007 <ayush.nashine4807@gmail.com>
2025-05-03 05:53:17 +0530

    Initial setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "MLTuneX",
+    version = "0.1.0",
+    author = "Ayush Nashine",
+    author_email = "ayush.nashine@gmail.com",
+    description = "A package for machine learning tuning and optimization.",
+    long_description = open("README.md").read(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/yourusername/MLTuneX",
+    packages = find_packages(),
+    install_requires = [
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "langchain",
+        "openai",
+        "langchain-openai",
+        "langchain-community",
+        "langchain-core",
+        "optuna",
+        "python-dotenv"
+    ],
+    python_requires='>=3.8',
+)
